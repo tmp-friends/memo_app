159266e83a1c919e62de365b75266e470c0bc197
t.terai <tomoya.terai01@gmail.com>
2020-03-13 13:12:27 +0900

    投稿一覧画面を作成＆レイアウトを調整(トップ画面、投稿一覧画面)

---
+++ db/migrate/20200304083017_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.text :title
+      t.text :fact
+      t.text :abstraction
+      t.text :diversion
+
+      t.timestamps
+    end
+  end
+end
