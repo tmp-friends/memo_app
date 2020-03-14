3f46c63f973c5e2ee4b8f19059f25244a14f681b
t.terai <tomoya.terai01@gmail.com>
2020-03-14 20:00:52 +0900

    新規メモページを作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,20 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post =Post.find_by(id: params[:id])
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(title: params[:title], fact: params[:fact], abstraction: params[:abstraction], diversion: params[:diversion])
+    if @post.save
+      flash[:notice] = "メモを作成しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/new")
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'posts/index' => "posts#index"
+  get 'posts/new' => "posts#new"
+  post "posts/create" => "posts#create"
   get 'posts/:id' => "posts#show"
 
   get '/' => "home#top"
