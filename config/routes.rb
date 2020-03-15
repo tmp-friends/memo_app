19318dbf75d2b5ca7e4eb4694df0ba24a3f95c86
t.terai <tomoya.terai01@gmail.com>
2020-03-15 13:19:09 +0900

    ユーザ一覧画面＆ユーザ編集画面＆新規登録画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-  get 'posts/index' => "posts#index"
+  get '/' => "home#top"
 
+  get 'posts/index' => "posts#index"
   get 'posts/new' => "posts#new"
   post "posts/create" => "posts#create"
-
   get 'posts/:id' => "posts#show"
-  
   get 'posts/:id/edit' => "posts#edit"
   post 'posts/:id/update' => "posts#update"
   post 'posts/:id/destroy' => "posts#destroy"
 
-  get '/' => "home#top"
+  get 'signup' => "users#new"
+  post 'users/create' => "users#create"
+  get 'users/index' => "users#index"
+  get 'users/:id' => "users#show"
+  get 'users/:id/edit' => "users#edit"
+  post 'users/:id/update' => "users#update"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_083017) do
+ActiveRecord::Schema.define(version: 2020_03_15_020946) do
 
   create_table "posts", force: :cascade do |t|
     t.text "title"
     t.text "fact"
     t.text "abstraction"
     t.text "diversion"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
