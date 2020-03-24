c1d8e50a05ac5ae1982a16ec92648a944c7cc105
t.terai <tomoya.terai01@gmail.com>
2020-03-24 11:21:34 +0900

    パスワードを暗号化

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_234536) do
+ActiveRecord::Schema.define(version: 2020_03_24_020425) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2020_03_23_234536) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_name"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
